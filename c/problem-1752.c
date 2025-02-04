ccfe10e714f9350018e5fce6d027bd59b5686ae0
Tiago Andrade <90466869+ehotigas@users.noreply.github.com>
2025-02-04 08:57:31 -0300

    new: problem 2, 4, 1752

--- c/problem-1752.c
+++ c/problem-1752.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+int check(int* nums, int numsSize) {
+    if (numsSize <= 2) return 1;
+    int decreasingCount = 0;
+    int lastNumber = nums[numsSize - 1];
+    for (int numsIdx = 0; numsIdx < numsSize; numsIdx++) {
+        if (nums[numsIdx] < lastNumber) decreasingCount++;
+        if (decreasingCount > 1) return 0;
+        lastNumber = nums[numsIdx];
+    }
+    return 1;
+}
 
 int main(void) {
-    printf("Hello, world!\n");
+    int nums[] = { 2,1,3,4 };
+    int result = check(nums, 5);
+    printf("Result: %d", result);
     return 0;
 }
