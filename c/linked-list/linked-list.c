1a515931eda2a85acf456897e3c63433c29f1399
Tiago Andrade <90466869+ehotigas@users.noreply.github.com>
2025-02-10 11:00:15 -0300

    new: remove method

--- c/linked-list/linked-list.c
+++ c/linked-list/linked-list.c
@@ -31,6 +31,16 @@ struct Node *findLast(struct List *list, struct Node *ref) {
     return findLast(list, ref->next);
 }
 
+struct Node *findNode(struct List *list, int idx) {
+    if (idx >= list->size || idx < 0) return NULL;
+    
+    struct Node *ref = list->root;
+    for (int indexRef = 0; indexRef < idx; indexRef++) {
+        ref = ref->next;
+    }
+    return ref;
+}
+
 void add(struct List *list, void *value) {
     if (list->root == NULL) {
         list->root = Node(value, NULL);
@@ -42,19 +52,51 @@ void add(struct List *list, void *value) {
     list->size++;
 }
 
-
 void *find(struct List *list, int idx) {
-    if (idx < 0 || idx > list->size - 1) return NULL;
-    struct Node *ref = list->root;
-    for (int listIdx = 0; listIdx < list->size; listIdx++) {
-        if (listIdx == idx) return ref->value;
-        ref = ref->next;
-    }
-    return NULL;
+    if (idx >= list->size || idx < 0) return NULL;
+    return findNode(list, idx)->value;
 }
 
-void remove(struct List *list, int idx) {
-    if (idx < 0 || idx > list->size - 1) return;
+void delete(struct List *list, int idx) {
+    if (list->size == 0) return;
+    if (idx == 0) {
+        struct Node *node = list->root;
+        if (list->size > 1) {
+            list->root = list->root->next;
+        }
+        else {
+            list->root = NULL;
+        }
+        free(node);
+        list->size--;
+        return;
+    }
+    if (idx == list->size - 1) {
+        free(findNode(list, idx));
+        findNode(list, idx - 1)->next = NULL;
+        list->size--;
+        return;
+    }
+    struct Node *beforeNode = findNode(list, idx - 1);
+    struct Node *node = beforeNode->next;
+    struct Node *afterNode = node->next;
+    beforeNode->next = afterNode;
+    free(node);
+    list->size--;
+}
+
+void printIntegerArray(struct List *list) {
+    
+    printf("[");
+    struct Node *ref = list->root;
+    for (int idx = 0; idx < list->size; idx++) {
+        int *value = ref->value;
+        printf("%d", *value);
+        if (idx != list->size - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
 }
 
 
@@ -66,7 +108,10 @@ int main (int argc, char **argv) {
     // printf("%d\n", (*test + 1));
     struct List *list = List();
     add(list, &value);
-    int *item = find(list, 0);
-    printf("%d\n", *item);
+    add(list, &value);
+    add(list, &value);
+    printIntegerArray(list);
+    // int *item = find(list, 0);
+    // printf("%d\n", *item);
     return 0;
 }
